24e27e616888edbf21b2e4436837c6560d37308b
Andres Arevalo <andresfelipear@gmail.com>
2023-05-07 00:27:49 -0700

    deleting some unused data

---
+++ config/plugins.js
@@ -0,0 +1,13 @@
+module.exports = ({ env }) => ({
+    // Content Manager plugin
+    "content-manager": {
+      enabled: true,
+      // Add any custom configuration options here
+    },
+    // Roles & Permissions plugin
+  "users-permissions": {
+    enabled: true,
+    // Add any custom configuration options here
+  },
+  });
+  
